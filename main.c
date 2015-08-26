3d3cfb852d47e524752c27e8dbd99c90a4728578
Hemant Makar <hemantpiraji.makar@xcubelabs.com>
2015-08-26 18:03:24 +0530

    modofied main function

--- main.c
+++ main.c
@@ -1,16 +1,9 @@
 #include "headers.h"
-unsigned int str_len(char *ptr){
-
-	char *p = ptr;
-	while(*p){
-
-		p++;
-	};
-	return p -ptr;
-}
-
 int main(int argc, char **argv) {
 
+	int x = 10, y = 20;
 	printf("%s length of %s is %d\n", __FUNCTION__, "PURPLE_TALK", str_len("PURPLE_TALK"));
+	printf("%s Addition of %d and %d is %d\n", __FUNCTION__, x, y, addition(x,y));
+	printf("%s Substration of %d and %d is %d\n", __FUNCTION__, x, y, substraction(y, x));
 	return 0;
 }
