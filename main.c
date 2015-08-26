99499a66501ae13badaa16aa3188cf4c848926f1
Hemant Makar <hemantpiraji.makar@xcubelabs.com>
2015-08-26 17:23:10 +0530

    intialise the main function

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+unsigned int str_len(char *ptr){
+
+	char *p = ptr;
+	while(*p){
+
+		p++;
+	};
+	return p -ptr;
+}
+
+int main(int argc, char **argv) {
+
+	printf("%s length of %s is %d\n", __FUNCTION__, "PURPLE_TALK", str_len("PURPLE_TALK"));
+	return 0;
+}
