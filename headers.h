6b5ae20b7f6e0c7d292f5d01427456db65ad48cb
Hemant Makar <hemantpiraji.makar@xcubelabs.com>
2015-08-26 18:04:06 +0530

    modified headers.h

--- headers.h
+++ headers.h
@@ -1,6 +1,7 @@
 #ifndef __HEADERS_H__
   #define __HEADERS_H__
   #include<stdio.h>
-  int addition(int x, int y);
-  
+  	unsigned int str_len(char *ptr);
+	int addition(int x, int y);
+  	int substraction(int x, int y);
 #endif
